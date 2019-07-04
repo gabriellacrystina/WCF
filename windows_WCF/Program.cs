3af388030bbd5e4efca1029e215614a24f2353d6
gabriellacrystina <gabriellacrystina@gmail.com>
2019-07-03 23:42:47 -0300

    Exemplo de consumidor de WCF

---
+++ windows_WCF/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace windows_WCF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wcf_validaEmail = new Wcf_Exemplo.Service1Client();
+
+            Console.WriteLine($"Este email éh: {wcf_validaEmail.ValidarEmail("gabi@teste.com.br")}");
+            Console.WriteLine($"Este email éh: {wcf_validaEmail.ValidarEmail("teste_teste.com.br")}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
